fd3eabaa6970eeff6f0a90b99f77544c5b64a547
shreyas <cshreyas88@gmail.com>
2018-06-28 17:10:04 -0400

    removed parameter functionality

--- model-training/src/resume_preprocessing.py
+++ model-training/src/resume_preprocessing.py
@@ -6,6 +6,7 @@ import re
 # get file names from command prompt
 keywords_file_name = "keywords.txt"
 csv_file = "../resources/resume_data.csv"
+resume_directory ="../resources/ranked-resumes"
 
 
 #class resume_preprocessing:
@@ -20,35 +21,35 @@ def count_matching_keywords(keywords, resume_words):
 	return match_count
 
 # MAIN FUNCTION
-def preprocess(resume_directory):
-	# read keywords from a file into a list
-	with open(keywords_file_name) as keywords_file:
-		keywords = keywords_file.read()
-	keywords = keywords.split()
 
-	# create csv file to store data
-	csv_file_obj = open(csv_file, 'w')
-	csv_writer = csv.writer(csv_file_obj, delimiter=',')
-	row = ["Index", "Score"]
-	for i in range(len(keywords)):
-		row.append("Keyword" + str(i + 1))
-	csv_writer.writerow(row)
+# read keywords from a file into a list
+with open(keywords_file_name) as keywords_file:
+	keywords = keywords_file.read()
+keywords = keywords.split()
 
-	index = 1
-	for resume in glob.glob(os.path.join(resume_directory, '*txt')):
-		with open(resume) as resume_file:
-			# store words in resume as a list
-			resume_words = resume_file.read()
-			resume_words = re.findall(r"[\w']+", resume_words)
-			score = re.search('(?<=-)\s?\d+', resume)
-			score = score.group(0).strip()
-			print(str(resume) + " " + score)
-			# find number of occurences of keywords in the resume
-			key_match_count = count_matching_keywords(keywords, resume_words)
-			# resume_list.append({'file_name': str(resume), 'word_list': resume_words, 'score' : score, 'keywords_match_count': key_match_count})
-			csv_writer.writerow([index] + [score] + key_match_count)
-		index += 1
+# create csv file to store data
+csv_file_obj = open(csv_file, 'w')
+csv_writer = csv.writer(csv_file_obj, delimiter=',')
+row = ["Index", "Score"]
+for i in range(len(keywords)):
+	row.append("Keyword" + str(i + 1))
+csv_writer.writerow(row)
 
-	csv_file_obj.close()
-	print('Done.')
-	return
+index = 1
+for resume in glob.glob(os.path.join(resume_directory, '*txt')):
+	with open(resume) as resume_file:
+		# store words in resume as a list
+		resume_words = resume_file.read()
+		resume_words = re.findall(r"[\w']+", resume_words)
+		score = re.search('(?<=-)\s?\d+', resume)
+		score = score.group(0).strip()
+		print(str(resume) + " " + score)
+		# find number of occurences of keywords in the resume
+		key_match_count = count_matching_keywords(keywords, resume_words)
+		# resume_list.append({'file_name': str(resume), 'word_list': resume_words, 'score' : score, 'keywords_match_count': key_match_count})
+		csv_writer.writerow([index] + [score] + key_match_count)
+	index += 1
+
+csv_file_obj.close()
+print('Done.')
+
